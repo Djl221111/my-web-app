5e92f9f1816f4bc99597911ae92732800cff301d
Djl <jldeng07@outlook.com>
2024-08-03 17:23:53 +0800

    feat: Add countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,31 @@
 "use client";
-import { useState } from "react";
+import Countdown from 'react-countdown';
 import { useWindowSize } from "react-use";
 import Confetti from "react-confetti";
 export default function Home() {
-  let [counter, setCounter] = useState(0);
   const{width,height}=useWindowSize();
-  function handleClick() {
-    setCounter(counter + 1);
-    console.log(counter);
+  interface RendererPrams{
+    hours: number,
+    minutes: number,
+    seconds: number,
+    completed: boolean
+  }
+  function renderer({ hours, minutes, seconds, completed } : RendererPrams){
+    if (completed){
+      return (<Confetti width={width} height={height} recycle={false} gravity={0.2}  wind={0} numberOfPieces={1000}/>);
+    }
+    else {
+      const hour =(hours>=10?`${hours}`:`0${hours}`);
+      const minute = (minutes>=10?`${minutes}`:`0${minutes}`);
+      const second = (seconds>=10?`${seconds}`:`0${seconds}`);
+      return (<span className="text-8xl font-mono font-bold">{hour}:{minute}:{second}</span>);
+    }
   }
   return (
     <main>
-      {
-        (counter>5) &&<Confetti width={width} height={height} recycle={false} gravity={0.2} wind={0} numberOfPieces={1000}/>
-      }
       <div className="w-screen h-screen flex items-center">
         <div className="m-auto">
-          <div>I have clicked {counter} times</div>
-          <div className="flex items-center">
-            <button
-              onClick={handleClick}
-              className="m-auto border rounded-lg p-2 bg-sky-400 
-                  text-white hover:bg-sky-600 active:bg-sky-700"
-            >
-              Click Me!
-            </button>
-          </div>
+          <Countdown date={Date.now()+5000} renderer={renderer}/>
         </div>
       </div>
     </main>
