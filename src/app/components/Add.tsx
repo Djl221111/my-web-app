74a6591b9cbf86dcae3c19b34efa853bfd231040
Djl <jldeng07@outlook.com>
2024-09-08 09:57:17 +0800

    feat: Add toast

--- src/app/components/Add.tsx
+++ src/app/components/Add.tsx
@@ -1,6 +1,8 @@
 import {TodoHandler} from "../todo/TodoHandler";
 import {Button} from"./Button";
 import {useState} from"react";
+import {ToastContainer, toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 interface AddArgs{
     todos: TodoHandler[];
     setTodos: (todos: TodoHandler[]) => void;
@@ -18,10 +20,15 @@ function Add({todos, setTodos}: AddArgs){
         setDate(date.getTime());
     }
     function addTodo(){
+        if(title===""||date===0){
+            toast.error("请输入代办内容和日期");
+            return;
+        }
         const nextTodoId = TodoHandler.getNextId();
         const todo = new TodoHandler(nextTodoId, title, date);
         todos.push(todo);
         setTodos(todos);
+        toast.success("添加成功");
     }
     return(
         <div className="flex-1 p-4 flex">
@@ -29,6 +36,7 @@ function Add({todos, setTodos}: AddArgs){
             <input type="text" placeholder="请输入代办内容" className="p-2 border rounded-lg w-1/3" onChange={setTitleOnChange}></input>
             <input type="date" className="p-2 border rounded-lg w-1/3" onChange={setDateOnChange}></input>
             <Button icon="✓" bgColor="bg-green-500" label="添加" onClick={addTodo}></Button>
+            <ToastContainer/>;
             </div>
         </div>
     );
