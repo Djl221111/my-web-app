1f9cac886f2d7a87a44749f31b699fd716ddf629
Djl <jldeng07@outlook.com>
2024-09-08 09:08:21 +0800

    feat: implement todos tomorrow and the day after tomorrow

--- src/app/components/Today.tsx
+++ src/app/components/Today.tsx
@@ -5,21 +5,29 @@ interface TodayArgs{
     todos: TodoHandler[];
 }
 function Today({todos} : TodayArgs){
-    const todosToday = todos.filter((todo,index)=>{
-        const date = new Date(todo.getExpireDate());
-        const now = new Date(Date.now());
-        if (date.getFullYear()===now.getFullYear()&&
-            date.getMonth()===now.getMonth()&&
-            date.getDay()===now.getDay()){
-                return todo;
-        }
+    const todosToday = todos.filter((todo, index)=>{
+        const dateTodo = new Date(todo.getExpireDate());
+        const dateToday = new Date(Date.now());
+        return dateTodo.toDateString()===dateToday.toDateString();
+    });
+    const todosTomorrow = todos.filter((todo, index)=>{
+        const dateTodo = new Date(todo.getExpireDate());
+        const dateToday = new Date(Date.now());
+        dateToday.setDate(dateToday.getDate()+1);
+        return dateTodo.toDateString()===dateToday.toDateString();
+    });
+    const todosTheDayAfterTomorrow = todos.filter((todo, index)=>{
+        const dateTodo = new Date(todo.getExpireDate());
+        const dateToday = new Date(Date.now());
+        dateToday.setDate(dateToday.getDate()+2);
+        return dateTodo.toDateString()===dateToday.toDateString();
     });
     return (
         <div className="flex-1 p-4">
             <h1 className="text-blue-600 font-bold text-4xl mb-6"> 今天 </h1>
             <div className="bg-white flex h-5/6 shadow-md rounded-xl divide-x divide-dashed">
-                <div className="w-1/3 text-2xl"> 
-                上午 
+                <div className="w-1/3 text-2xl overflow-scroll p-4 space-y-2"> 
+                今天 
                 <div className="space-y-2 p-2">
                     {
                         todosToday.map((todo, index) => {
@@ -30,8 +38,28 @@ function Today({todos} : TodayArgs){
                     }
                 </div>
                 </div>
-                <div className="w-1/3 text-2xl"> 明天 </div>
-                <div className="w-1/3 text-2xl"> 后天 </div>
+                <div className="w-1/3 text-2xl overflow-scroll p-4 space-y-2"> 明天 
+                <div>
+                    {
+                        todosTomorrow.map((todo,index)=>{
+                            return (
+                                <TodoComponent key={index} title={todo.getTitle()} expireDate={todo.getExpireDate()}/>
+                            );
+                        })
+                    }
+                </div>
+                </div>
+                <div className="w-1/3 text-2xl overflow-scroll p-4 space-y-2"> 后天 
+                <div>
+                    {
+                        todosTheDayAfterTomorrow.map((todo,index)=>{
+                            return (
+                                <TodoComponent key={index} title={todo.getTitle()} expireDate={todo.getExpireDate()}/>
+                            );
+                        })
+                    }
+                </div>
+                </div>
             </div>
         </div>
     );
